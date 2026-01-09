058d2cb9a7c3ddb85dfb6a54f632a45a1f51e00b
mt-creations <mthomas92551@gmail.com>
2026-01-09 08:39:20 -0800

    week 1 exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,11 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    { 
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your names is {lastName}, {firstName} {lastName}.");        
     }
 }
