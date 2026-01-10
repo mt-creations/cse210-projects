4742a8c940574013737a33a653657953c2bfc232
mt-creations <mthomas92551@gmail.com>
2026-01-09 18:12:36 -0800

    week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        
+        Console.Write("What is your guess? ");
+        string numberGuess = Console.ReadLine();
+        int guess = int.Parse(numberGuess);
+        int amountOfGuesses = 1;
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Guess higher. ");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Guess lower. ");
+            }
+
+            Console.Write("What is your guess? ");
+            numberGuess = Console.ReadLine();
+            guess = int.Parse(numberGuess);
+            amountOfGuesses++;
+        }  
+        Console.WriteLine($"You guessed it in {amountOfGuesses} tries!");  
     }
 }
