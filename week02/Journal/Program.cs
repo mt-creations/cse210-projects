5813470b0693c81be4f5374709c920a90f3796cb
mt-creations <mthomas92551@gmail.com>
2026-01-12 21:02:43 -0800

    starting journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        string selection = "0";
+
+        while (selection != "5")
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("Please select one of the following choices.");
+            selection = Console.ReadLine();
+        }
     }
 }
