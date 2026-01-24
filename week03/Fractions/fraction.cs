ea09a26e0fbdca24305621523c641d5506d7afab
mt-creations <mthomas92551@gmail.com>
2026-01-24 13:59:49 -0800

    fractions

---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,63 @@
+
+
+
+using System.Diagnostics;
+
+public class Fraction
+{
+//fields
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+//constructors
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+// getters and setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+//return methods
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+
+}
