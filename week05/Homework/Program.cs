6cf8a3e4e0cd13d3bb287f71520d75316a3a780d
mt-creations <mthomas92551@gmail.com>
2026-02-03 14:01:38 -0800

    week4 activity 1

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John", "math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Joe", "subtraction", "7", "1-20");
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 =new WritingAssignment("Jennifer", "History", "US History 1800 - 1900");
+        Console.WriteLine(writing1.GetWritingInformation());
+
     }
 }
