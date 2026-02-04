02a79026d8f9dd89cd4754979c8241ad1b803a0e
mt-creations <mthomas92551@gmail.com>
2026-02-03 21:14:02 -0800

    mindfulness start

---
+++ week05/Mindfulness/listingActivity.cs
@@ -0,0 +1,34 @@
+
+
+
+
+public class ListeningActivity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListeningActivity(int count, List<string> prompts)
+    {
+        _count = count;
+        _prompts = prompts;
+    }
+
+    public void Run()
+    {
+        
+    }
+
+    public void GetRandomPrompt()
+    {
+        
+    }
+
+    public List<string> GetListFromUser()
+    {
+        
+    }
+
+
+
+
+}
---
+++ week05/Mindfulness/reflectingActivity.cs
@@ -0,0 +1,45 @@
+
+
+
+
+using System.Diagnostics.Contracts;
+
+public class ReflectionActivity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectionActivity(List<string> prompts, List<string> questions)
+    {
+        List<string> _prompts = prompts;
+        List<string> _questions = questions;
+    }
+    public void Run()
+    {
+        
+    }
+    public string GetRandomPrompt()
+    {
+        
+    }
+
+    public string GetRandomQuestion()
+    {
+        
+    }
+
+    public void DisplayPrompt()
+    {
+        
+    }
+
+    public void DisplayQuestion()
+    {
+        
+    }
+
+
+
+
+
+}
