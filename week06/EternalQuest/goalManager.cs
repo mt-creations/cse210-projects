0158b4ddaa05d9030fef6ad52c5911e486c1fbf6
mt-creations <mthomas92551@gmail.com>
2026-02-10 20:13:47 -0800

    goal

--- week06/EternalQuest/goalManager.cs
+++ week06/EternalQuest/goalManager.cs
@@ -9,12 +9,6 @@ public class GoalManager
     private List<Goal> _goals = new List<Goal>();
     private int _score = 0;
 
-    // public GoalManager()
-    // {
-    //     _goals = new List<Goal>();
-    //     _score = 0;
-    // }
-
     public int GetScore()
     {
         return _score;
